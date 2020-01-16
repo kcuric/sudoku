6b10f805bd2da427ad71ef3c1a6d1766e7b785f7
Krešimir Ćurić <kresimir.curic1@gmail.com>
2020-01-16 19:00:25 +0100

    #1 Separated the sudoku to a class
    Sudoku is now a class with a method get_empty_board. I've also added a few things in .gitignore.

--- sudoku/app.py
+++ sudoku/app.py
@@ -1,11 +1,5 @@
-def get_empty_board(size: int) -> list:
-    '''
-    Generates an empty sudoku board.
-    '''
-    board = list()
-    for _ in range(size):
-        board.append([0 for x in range(size)])
-    return board
+from sudoku.modules.sudoku import Sudoku
 
 def run():
-    print(get_empty_board(9))
+    sudoku = Sudoku()
+    print(sudoku.get_empty_board(9))
---
+++ sudoku/modules/sudoku.py
@@ -0,0 +1,19 @@
+class Sudoku:
+
+    def __init__(self):
+        pass
+
+    def get_empty_board(self, size: int) -> list:
+        '''
+        Generates an empty sudoku board.
+
+        Parameters:
+        size (int): Size of the Sudoku board (size x size).
+
+        Returns:
+        list: An empty Sudoku board as a 2D list.
+        '''
+        self.board = list()
+        for _ in range(size):
+            self.board.append([0 for x in range(size)])
+        return self.board
